e6a4f480207731e84ba0fa64a6ca5e891a7f3e81
maria-robobug <mariashow2001@gmail.com>
2019-03-20 20:19:31 +0000

    Added Greet method to server

--- greet/greetserver/server.go
+++ greet/greetserver/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -10,6 +11,17 @@ import (
 )
 
 type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello" + firstName
+
+	resp := &greetpb.GreetResponse{
+		Result: result,
+	}
+
+	return resp, nil
+}
 
 func main() {
 	fmt.Println("Hello World")
